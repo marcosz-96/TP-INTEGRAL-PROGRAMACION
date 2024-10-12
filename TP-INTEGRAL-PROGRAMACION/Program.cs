eb0c3d11adece8a8942d8751ce736d022f84e876
marcosz-96 <168491483+marcosz-96@users.noreply.github.com>
2024-10-12 16:23:25 -0300

    Modificaciones y agregados

--- TP-INTEGRAL-PROGRAMACION/Program.cs
+++ TP-INTEGRAL-PROGRAMACION/Program.cs
@@ -14,6 +14,23 @@ namespace TP_INTEGRAL_PROGRAMACION
         static bool existe = false;
         static int opcion;
         static string productoBuscado, productoAEliminar;
+        
+        public struct Cerveza
+        {
+            public string NombreCerveza;
+            public string PresentacionCerveza;
+            public int StockCerveza;
+            public double PreciosCerveza;
+            
+
+            public Cerveza(string nombreCerveza, string presentacionCerveza, int stockCerveza, double preciosCerveza)
+            {
+                NombreCerveza = nombreCerveza;
+                PresentacionCerveza = presentacionCerveza;
+                StockCerveza = stockCerveza;
+                PreciosCerveza = preciosCerveza;
+            }
+        }
         static void Main(string[] args)
         {
             Cerveza[] infoDeCerveza = new Cerveza[]
@@ -28,25 +45,25 @@ namespace TP_INTEGRAL_PROGRAMACION
                 new Cerveza("HONEY", "BARRILES", 29, 112000.00),
             };
 
-            Console.WriteLine("Bienvenido al programa de Gestión de Inventario;");
+            Console.WriteLine("Bienvenido al programa de Gestión de Inventario!");
             do
             {
                 MensajeYMenu();
                 opcion = int.Parse(Console.ReadLine());
-                IngresoAlMenu(opcion, infoDeCerveza);
+                IngresoAlMenu(opcion, infoDeCerveza, productoAEliminar);
             } while (opcion != 6);
         }
         static void MensajeYMenu()
         { 
-            Console.WriteLine("\nA partir del siguiente menú elija una opción:");
+            Console.WriteLine("\nElija una opción del siguiente menú :");
             Console.WriteLine("Opcion 1: buscar productos");
             Console.WriteLine("Opcion 2: ver información productos");
             Console.WriteLine("Opcion 3: agregar producto");
             Console.WriteLine("Opcion 4: eliminar producto");
             Console.WriteLine("Opcion 5: realizar presupuesto");
-            Console.WriteLine("Opcion 6: salir");
+            Console.WriteLine("Opcion 6: salir\n");
         }
-        static void IngresoAlMenu(int opcion, Cerveza[] infoDeCerveza) 
+        static void IngresoAlMenu(int opcion, Cerveza[] infoDeCerveza, string productoAEliminar) 
         {
             switch (opcion)
             {
@@ -58,7 +75,12 @@ namespace TP_INTEGRAL_PROGRAMACION
                     break;
                 case 3:
                 case 4:
-                    EliminarProducto(infoDeCerveza);
+                    EliminarProducto(infoDeCerveza, productoAEliminar);
+                    StockConProductoEliminado(infoDeCerveza);
+                    break;
+                case 5:
+                case 6:
+                    Console.WriteLine("Fin del programa");
                     break;
                 default: Console.WriteLine("Opcion invalida");break;
             } 
@@ -80,13 +102,13 @@ namespace TP_INTEGRAL_PROGRAMACION
             }
             if (!existe)
             {
-                Console.WriteLine("El producto no se encontró");
+                Console.WriteLine("El producto no se encuentra disponible");
             }
             
         }
         static void VerInformacionDeProductos(Cerveza[] infoDeCerveza)
         {
-            Console.WriteLine("\nLos Productos disponibles son:");
+            Console.WriteLine("\nLos Productos disponibles son:\n");
             foreach (Cerveza cerveza in infoDeCerveza)
             {
                 Console.WriteLine($"Nombre: {cerveza.NombreCerveza}, " +
@@ -95,51 +117,37 @@ namespace TP_INTEGRAL_PROGRAMACION
                     $"Precio: {cerveza.PreciosCerveza}");
             }
         }
-        static void EliminarProducto(Cerveza[] infoDeCerveza)
-        { 
-            Console.Write("\nIndique el nombre del producto que desea eliminar del stock: ");
-            productoAEliminar = Console.ReadLine().ToUpper();
-
-            int indiceAEliminar = Array.FindIndex(infoDeCerveza, e => e.NombreCerveza.ToUpper() == productoAEliminar);
-
+        static Cerveza[] EliminarProducto(Cerveza[] infoDeCerveza, string productoAEliminar)
+        {
+            int indiceAEliminar = Array.FindIndex(infoDeCerveza, e => e.NombreCerveza.Equals(productoAEliminar, StringComparison.OrdinalIgnoreCase));
+            
             if (indiceAEliminar != -1)
             {
                 Cerveza[] nuevaInfoDeCerveza = new Cerveza [infoDeCerveza.Length -1];
 
                 Array.Copy(infoDeCerveza, 0, nuevaInfoDeCerveza, 0, indiceAEliminar);
                 Array.Copy(infoDeCerveza, indiceAEliminar + 1, nuevaInfoDeCerveza, indiceAEliminar, infoDeCerveza.Length - indiceAEliminar - 1);
+                return nuevaInfoDeCerveza;
+            }
+            return infoDeCerveza;
+        }
+        static void StockConProductoEliminado(Cerveza[] infoDeCerveza)
+        {
+            Console.Write("\nIngrese el nombre del producto que desea eliminar: ");
+            string productoAEliminar = Console.ReadLine();
 
-                infoDeCerveza = nuevaInfoDeCerveza;
-                //return nuevaInfoDeCerveza;
-            }
-            else
-            {
-                Console.WriteLine("El producto no se encontró");
-                //return infoDeCerveza;
-            }
-            Console.WriteLine("La lista con el producto eliminado es:");
+            infoDeCerveza = EliminarProducto(infoDeCerveza, productoAEliminar);
+
+            Console.WriteLine("\nEl stock actualizado es:");
             foreach (Cerveza nuevaLista in infoDeCerveza)
             {
-                Console.WriteLine(nuevaLista + " ");
+                Console.WriteLine($"Nombre: {nuevaLista.NombreCerveza}, " +
+                    $"Presentación: {nuevaLista.PresentacionCerveza}, " +
+                    $"Stock: {nuevaLista.StockCerveza}, " +
+                    $"Precio: {nuevaLista.PreciosCerveza}");
             }
         }
-
-        public struct Cerveza 
-        {
-            public string  NombreCerveza;
-            public string PresentacionCerveza;
-            public int StockCerveza;
-            public double PreciosCerveza;
-
-            public Cerveza (string nombreCerveza, string presentacionCerveza, int stockCerveza, double preciosCerveza)
-            {
-                NombreCerveza = nombreCerveza;
-                PresentacionCerveza = presentacionCerveza;
-                StockCerveza = stockCerveza;
-                PreciosCerveza = preciosCerveza;
-            }
-        }
-
+        
     }
 
 }
